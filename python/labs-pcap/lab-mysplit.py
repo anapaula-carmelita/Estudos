760527b87fbd74625fc683dc4bc65e5d1ebc7b63
Ana Paula da Silva Souza <110702993+anapaula-carmelita@users.noreply.github.com>
2026-07-17 20:32:08 -0300

    incluindo o método
    inclusão da primeira versão do método do split

--- python/labs-pcap/lab-mysplit.py
+++ python/labs-pcap/lab-mysplit.py
@@ -8,8 +8,19 @@
 #
 
 def mysplit(strng):
-    
+    strng = strng.strip()
+    if strng == '':
+        return ''
 
+    lista = [] 
+    s =''
+    for c in strng:
+        if c == ' ':
+            lista.append(s)
+            s = ''
+        s += c
+    lista.append(s)
+    return s
 
 print(mysplit("To be or not to be, that is the question"))
 print(mysplit("To be or not to be,that is the question"))
