29e93b968ccc50cac2dd14d51d5f525a92ec292d
Ana Paula da Silva Souza <110702993+anapaula-carmelita@users.noreply.github.com>
2026-07-17 17:01:26 +0000

    inclusao do template

---
+++ python/labs-pcap/lab-mysplit.py
@@ -0,0 +1,18 @@
+#
+#  Nome do arquivo: mysplit.py
+#  Descrição: Programa de uma possível solução do LAB 'You own split' do livro Python Essentials 2
+#  Autor: Ana Paula da Silva Souza
+#  Data: 17/07/2026
+#  Versão: 1.0
+#  Licença: Apache
+#
+
+def mysplit(strng):
+    
+
+
+print(mysplit("To be or not to be, that is the question"))
+print(mysplit("To be or not to be,that is the question"))
+print(mysplit(" "))
+print(mysplit(" abc "))
+print(mysplit(""))
